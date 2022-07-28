d1702c803f200058ea5fd06544e25087d66e6046
Rordigo <rodrigokuong.gmail.com>
2022-07-28 17:32:28 -0300

    Primer Commit

---
+++ src/Components/Container.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+
+const api = {
+    key: '97edc8bd2f51ffd99517bf6568e95789',
+    base: 'https://api.openweathermap.org/data/2.5/'
+}
+export const Container = () => {
+
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+    const search = evt => {
+        if (evt.key === "Enter") {
+            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+                .then(res => res.json())
+                .then(result => {
+                    setWeather(result);
+                    setQuery('');
+                    console.log(result)
+                })
+        }
+    }
+    const dateBuilder = (d) => {
+        const months = ["January", "Febrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+        const days = ["Sunday", "Monday", "Tuesday", "Wendnesday", "Thrusday", "Friday", "Saturday"];
+
+        const day = days[d.getDay()];
+        const date = d.getDate();
+        const month = months[d.getMonth()];
+        const year = d.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`
+    }
+    return (
+        <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+            <main>
+                <div className='search-box'>
+                    <input
+                        type='text'
+                        className='search-bar'
+                        placeholder='Search...'
+                        onChange={e => setQuery(e.target.value)}
+                        value={query}
+                        onKeyPress={search}
+                    />
+                </div>
+                {(typeof weather.main != "undefined") ? (
+                <div>
+                    <div className='location-box'>
+                        <div className='location'>{weather.name}, {weather.sys.country}</div>
+                        <div className='date'>{dateBuilder(new Date())}</div>
+                    </div>
+                    <div className='weather-box'>
+                        <div className='temp'>{Math.round(weather.main.temp)}°C</div>
+                        <div className='weather'>{weather.weather[0].main}</div>
+                    </div>
+                </div>
+                    ) : ('')}
+            </main>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Container } from './Components/Container';
+import './Styles/index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Container />
+  </React.StrictMode>
+);
+
